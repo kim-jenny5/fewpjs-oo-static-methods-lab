3ee97400a0a1a89c12ccd14527cd34e97199b8cf
Jenny Kim <kimj1905@gmail.com>
2021-09-20 15:10:46 -0400

    commit tree 6c2a61576674e0e92b03381b7074233e2818cbbe

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1, str.length);
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z-'\d\s]/g, "");
+  }
+
+  static titleize(str) {
+    const sentenceArr = str.split(" ");
+    const afterFirstWord = sentenceArr.slice(1, sentenceArr.length);
+    const firstWord = this.capitalize(sentenceArr[0]);
+    const doNotCapitalize = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+    const self = this;
+    // const newSentence = afterFirstWord.map(function (word) {
+    //   return !doNotCapitalize.includes(word) ? self.capitalize(word) : word;
+    // });
+    const newSentence = afterFirstWord.map((word) =>
+      !doNotCapitalize.includes(word) ? self.capitalize(word) : word
+    );
+    return firstWord + " " + newSentence.join(" ");
+  }
 }
